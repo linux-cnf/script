dff73305108e751a60a13c58f64b6c7e80db4945
linux-cnf <linux.cnf@gmail.com>
2022-08-10 23:59:43 +0530

    automation of docker installation

---
+++ docker.sh
@@ -0,0 +1,16 @@
+#how to install docker in ubuntu by bash script automation
+#author: Samiksha Shamra
+sudo apt update -y
+sudo apt -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker-archive-keyring.gpg
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update -y
+sudo apt install docker-ce docker-ce-cli containerd.io -y
+sudo usermod -aG docker ubuntu
+sudo chmod 666 /var/run/docker.sock
+sudo apt install git -y
+#sudo apt install gh -y
+sudo apt install unzip -y
+sudo systemctl start docker
+sudo systemctl enable docker
+docker --version

5f13a9f066c39bf79dd81402cff8f39e4be81202
linux-cnf <linux.cnf@gmail.com>
2022-11-25 16:44:03 +0530

    fixing

--- sqsmigration.sh
+++ sqsmigration.sh
@@ -5,7 +5,7 @@
 
 mkdir sqs;cd sqs
 #listing SQS URLs and names in a single file(urlname) from Source aws account
-aws sqs list-queues --profile source_aws_account --region us-west-2 |grep -i http |sed -e 's/"//g'|sed -e 's/,//g' > urlfile ;aws sqs list-queues --profile ec --region us-west-2 |grep -i http |sed -e 's/"//g'|sed -e 's/,//g' |awk -F '/' '{print $NF}' > listname ; paste urlfile listname > urlname
+aws sqs list-queues --profile source_aws_account --region us-west-2 |grep -i http |sed -e 's/"//g'|sed -e 's/,//g' > urlfile ;aws sqs list-queues --profile source_aws_account --region us-west-2 |grep -i http |sed -e 's/"//g'|sed -e 's/,//g' |awk -F '/' '{print $NF}' > listname ; paste urlfile listname > urlname
 #Queues configuration collected here
 cat urlname|while read a b ; do aws sqs get-queue-attributes --queue-url $a  --profile source_aws_account --region us-west-2 --attribute-names VisibilityTimeout MaximumMessageSize MessageRetentionPeriod DelaySeconds ReceiveMessageWaitTimeSeconds SqsManagedSseEnabled| grep -v '"Attributes": {' > $b.json ; done 
 #set json files accordingly.

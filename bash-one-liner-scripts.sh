7ff59b7adb1d70c572252e58143283a572368521
linux-cnf <linux.cnf@gmail.com>
2022-11-28 17:03:44 +0530

    secomd commit

--- bash-one-liner-scripts.sh
+++ bash-one-liner-scripts.sh
@@ -5,3 +5,9 @@
 #listing all the files recusrively with size of files of multiple folders.
 #Note: Kindly change folders location according to your demand.
 /usr/bin/du -a /var/log /home/user/log |sort -nr |awk '{print $NF}'|while read l ;do echo "file $(du -csh $l|grep -vi total) " ;done|nl
+
+#Kindly try below one line bash script to generate random strong password.
+cat /dev/urandom |tr -dc "[[:graph:]]" |head -c16;echo
+
+#it worked for me to list all cron jobs for all users.
+cat /etc/passwd |awk -F ':' '{print $1}'|while read a;do crontab -l -u ${a} ; done

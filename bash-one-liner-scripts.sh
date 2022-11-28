3ff63d0386c1447f687c058b9806710057fc7908
linux-cnf <linux.cnf@gmail.com>
2022-11-28 17:05:27 +0530

    one liner bash scripts

--- bash-one-liner-scripts.sh
+++ bash-one-liner-scripts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #purpose: writing only one liner bash shell scripts
 #contact: linux.cnf@gmail.com
-
+#author: Bablish Jaiswal
 #listing all the files recusrively with size of files of multiple folders.
 #Note: Kindly change folders location according to your demand.
 /usr/bin/du -a /var/log /home/user/log |sort -nr |awk '{print $NF}'|while read l ;do echo "file $(du -csh $l|grep -vi total) " ;done|nl

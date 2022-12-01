b6b1e0ed6e680a24472a7fa2feb01f39db0cd53b
linux-cnf <linux.cnf@gmail.com>
2022-12-02 03:03:57 +0530

    one liner bash script

--- bash-one-liner-scripts.sh
+++ bash-one-liner-scripts.sh
@@ -12,3 +12,10 @@ cat /dev/urandom |tr -dc "[[:graph:]]" |head -c16;echo
 
 #it worked for me to list all cron jobs for all users.
 cat /etc/passwd |awk -F ':' '{print $1}'|while read a;do crontab -l -u ${a} ; done
+
+#error "Argument list too long" while deleting/null lots of files.
+cd target_location
+#below script to turns file size zero/blank
+for i in $(ls) ; do cat /dev/null/ > $i ; done
+#below script to delete
+for i in * ; do cat rm -rfv $i ; done
